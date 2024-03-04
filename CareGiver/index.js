49c96652997aba4e55329539478dab31c52237be
RobertRobern <ndolorobern@gmail.com>
2024-03-04 14:13:40 +0300

    updates

--- CareGiver/index.js
+++ CareGiver/index.js
@@ -49,7 +49,7 @@ let map;
 
 async function initMap() {
   // The location of Uluru
-  const position = { lat: -0.2833, lng: 36.8166 };
+  const position = { lat: -0.772928, lng: 34.5993411 };
   // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
